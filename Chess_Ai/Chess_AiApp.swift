a5e9a919dd9c7ecf2cd2c51792507446fd0483c0
Le Minh Dang <dangleminh6677@gmail.com>
2026-04-01 01:28:50 +0700

    Initial Commit

---
+++ Chess_Ai/Chess_AiApp.swift
@@ -0,0 +1,17 @@
+//
+//  Chess_AiApp.swift
+//  Chess_Ai
+//
+//  Created by Lê Minh Đăng on 1/4/26.
+//
+
+import SwiftUI
+
+@main
+struct Chess_AiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
